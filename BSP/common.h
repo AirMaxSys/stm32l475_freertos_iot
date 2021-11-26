84377af0b96a9ab547f8ebfc955293a7d0d9d35b
AirMaxSys <36151808+AirMaxSys@users.noreply.github.com>
2021-11-26 12:38:32 +0800

    update

--- BSP/common.h
+++ BSP/common.h
@@ -1,12 +1,12 @@
 #ifndef _COMMON_H_
 #define _COMMON_H_
 
-#include "main.h"
 #include <stdint.h>
+#include "stm32l475xx.h"
 
 typedef struct gpio {
     GPIO_TypeDef *port;
-    uint32_t pin;
+    uint16_t pin;
 } gpio_t;
 
 #endif
