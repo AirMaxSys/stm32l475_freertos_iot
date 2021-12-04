895204ea142de3530bd23476bd99de7536825237
AirMaxSys <36151808+AirMaxSys@users.noreply.github.com>
2021-12-04 10:49:37 +0800

    adjust mian stack size and FreeRTOS heap size

--- Config/FreeRTOSConfig.h
+++ Config/FreeRTOSConfig.h
@@ -56,7 +56,7 @@ build, this configuration file has all options turned on. */
 #define configCPU_CLOCK_HZ						80000000
 #define configMAX_PRIORITIES					( 32 )
 #define configMINIMAL_STACK_SIZE				( ( unsigned short ) 256 )
-#define configTOTAL_HEAP_SIZE					( ( size_t ) ( 60 * 1024 ) )
+#define configTOTAL_HEAP_SIZE					( ( size_t ) ( 45 * 1024 ) )
 #define configMAX_TASK_NAME_LEN					( 10 )
 #define configUSE_TRACE_FACILITY				1
 #define configUSE_16_BIT_TICKS					0
